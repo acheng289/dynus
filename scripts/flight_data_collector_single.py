26446ccced07e85f397d7797743360b5771d933e
averageandyyy <averageandyyy@gmail.com>
2025-05-21 15:17:34 +0800

    edits to data collector

--- scripts/flight_data_collector_single.py
+++ scripts/flight_data_collector_single.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile
@@ -24,7 +26,7 @@ class StateGoalCollector(Node):
 def main(args=None):
     rclpy.init(args=args)
     node = StateGoalCollector()
-    rclpu.spin(node)
+    rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
 
